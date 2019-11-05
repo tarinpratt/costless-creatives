95117f932540229c124032a339c5a09005ed81b8
Tarin Pratt <tarinpratt@gmail.com>
2019-11-04 17:20:20 -0700

    post

---
+++ src/AddPost/AddPost.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import AddPost from './AddPost';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter><AddPost /></BrowserRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
